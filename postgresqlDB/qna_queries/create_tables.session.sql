038fab67c2adde47533977a8b1d2b54902cb3ea4
sathya2931 <sathyamoorthi2k01@gmail.com>
2021-10-21 17:03:53 +0530

    Added postgres files

---
+++ postgresqlDB/qna_queries/create_tables.session.sql
@@ -0,0 +1,32 @@
+-- @block create table users
+CREATE TABLE users (
+   roll_number INT PRIMARY KEY,
+   user_name VARCHAR(50) UNIQUE,
+   creation_date DATE,
+   about_me TEXT,
+   email VARCHAR(50) NOT NULL
+);
+
+-- @block create table login_details
+CREATE TABLE login_details(
+   roll_number INT PRIMARY KEY REFERENCES users(roll_number),
+   password_hash TEXT
+);
+
+-- @block create table questions 
+CREATE TABLE questions(
+   id SERIAL PRIMARY KEY,
+   creation_date DATE,
+   question_text TEXT,
+   roll_number INT REFERENCES users(roll_number),
+   answer_count SMALLINT
+);
+
+-- @block create table answers
+CREATE TABLE answers(
+   id SERIAL PRIMARY KEY,
+   question_id INT REFERENCES questions(id),
+   answer_text TEXT,
+   creation_date DATE,
+   roll_number INT REFERENCES users(roll_number)
+);
---
+++ postgresqlDB/qna_queries/drop_delete_tables.session.sql
@@ -0,0 +1,25 @@
+-- @block drop users table
+DROP TABLE users;
+-- @block drop login_details table
+DROP TABLE login_details;
+-- @block drop answers table
+DROP TABLE answers;
+-- @block drop questions table
+DROP TABLE questions;
+
+
+
+--@block delete entries from users
+DELETE FROM users;
+
+
+--@block delete entries from login_details
+DELETE FROM login_details;
+
+
+--@block delete entries from questions
+DELETE FROM questions;
+
+
+--@block delete entries from answers
+DELETE FROM answers;
